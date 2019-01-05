729406654bbfbf0a11265501e5c38085777b5921
Brian Celenza <bcelenza@gmail.com>
2019-01-04 18:40:57 -0800

    Move light control loop to its own file
    Status polling is now working thanks to @abates

---
+++ lightcontrol.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"github.com/abates/insteon"
+	"github.com/abates/insteon/plm"
+	"log"
+	"time"
+)
+
+type LightControl struct {
+	location *Location
+	lighting *Lighting
+	plm *plm.PLM
+}
+
+func NewLightControl(location *Location, lighting *Lighting, plm *plm.PLM) *LightControl {
+	return &LightControl{
+		location:location,
+		lighting:lighting,
+		plm:plm,
+	}
+}
+
+// Start the lighting control loop with a given duration. The done channel is currently not implemented.
+func (lc *LightControl) Start(interval time.Duration, doneChan chan bool) {
+	for {
+		log.Println("Updating lighting brightness")
+
+		// get the latest outdoor scene
+		now := time.Now()
+		outdoorScene, err := lc.location.GetOutdoorScene(now)
+		if err != nil {
+			log.Printf("Unable to get OutdoorScene for this interval: %s\n", err)
+			time.Sleep(1 * time.Minute)
+			continue
+		}
+
+		for _, light := range lc.lighting.Lights {
+			if light.Type != Dimmer.String() {
+				log.Printf("Light '%s' is not a dimmer, skipping\n", light.Name)
+				continue
+			}
+
+			lc.setBrightness(light, outdoorScene)
+		}
+
+		time.Sleep(interval)
+	}
+}
+
+func (*LightControl) setBrightness(light Light, scene *OutdoorScene) {
+	desiredBrightnessPercent, err := light.DesiredBrightnessPercent(scene)
+	if err != nil {
+		log.Printf("Could not get desired brightness for light '%s': %s\n", light.Name, err)
+		return
+	}
+
+	log.Printf("Setting desired brightness for light '%s' to %f\n", light.Name, desiredBrightnessPercent*100)
+	for _, address := range light.InsteonOptions.Addresses {
+		addr := insteon.Address{}
+		err := addr.UnmarshalText([]byte(address))
+		if err != nil {
+			log.Printf("Could not parse insteon address for light '%s', address %s: %s\n", light.Name, address, err)
+			return
+		}
+
+		device, err := modem.Network.Connect(addr)
+		if err != nil {
+			log.Printf("Could not connect to device at address %s: %s\n", address, err)
+			return
+		}
+
+		if dimmer, ok := device.(insteon.Dimmer); ok {
+			// Determine the onLevel
+			onLevel := int(desiredBrightnessPercent * 255)
+			log.Printf("Calculated onLevel for device at address %s is %d\n", address, onLevel)
+
+			// Set the default onLevel
+			err = dimmer.SetDefaultOnLevel(onLevel)
+			if err != nil {
+				log.Printf("Could not set default OnLevel for device at address %s: %s\n", address, err)
+			}
+
+			// If the light is turned on, optionally adjust it's brightness
+			currentOnLevel, err := dimmer.Status()
+			if err != nil {
+				log.Printf("Could not determing current OnLevel for device at address %s: %s\n", address, err)
+				return
+			}
+
+			log.Printf("Current onlevel for address %s is %d\n", address, currentOnLevel)
+			if currentOnLevel > 0 && ((!scene.AfterMeridian && currentOnLevel < onLevel) || (scene.AfterMeridian && currentOnLevel > onLevel)) {
+				log.Printf("Adjusting current OnLevel of device at address %s to %d\n", address, onLevel)
+				err = dimmer.OnAtRamp(onLevel, 60)
+				if err != nil {
+					log.Printf("Could not adjust OnLevel of device at address %s: %s\n", address, err)
+					return
+				}
+			}
+		} else {
+			log.Printf("Device at address %s is not a dimmer", address)
+			return
+		}
+	}
+}
--- main.go
+++ main.go
@@ -7,17 +7,19 @@ import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
+	"os"
+	"os/signal"
 	"time"
 )
 
 var modem *plm.PLM
 
 func main() {
-	insteon.Log.Level(insteon.LevelDebug)
+	insteon.Log.Level(insteon.LevelTrace)
 	log.Println("Hello")
 
 	// parse home file
-	home := Location{}
+	home := &Location{}
 	homeFile, err := ioutil.ReadFile("config/home.yaml")
 	if err != nil {
 		log.Fatalf("Unable to open home configuration file: %s\n", err)
@@ -28,7 +30,7 @@ func main() {
 	}
 
 	// parse lighting file
-	lighting := Lighting{}
+	lighting := &Lighting{}
 
 	lightingFile, err := ioutil.ReadFile("config/lighting.yaml")
 	if err != nil {
@@ -53,76 +55,21 @@ func main() {
 	modem = plm.New(plm.NewPort(s, 5 * time.Second), 5 * time.Second)
 	defer modem.Close()
 
-	for {
-		// get the latest outdoor scene
-		now := time.Now()
-		outdoorScene, err := home.GetOutdoorScene(now)
-		if err != nil {
-			log.Printf("Unable to get OutdoorScene for this interval: %s\n", err)
-			time.Sleep(1 * time.Minute)
-			continue
-		}
+	// Start lighting control loop
+	lightControlDone := make(chan bool)
+	lightControl := NewLightControl(home, lighting, modem)
+	go lightControl.Start(5 * time.Minute, lightControlDone)
 
-		for _, light := range lighting.Lights {
-			if light.Type != Dimmer.String() {
-				log.Printf("Light '%s' is not a dimmer, skipping\n", light.Name)
-				continue
-			}
 
-			desiredBrightnessPercent, err := light.DesiredBrightnessPercent(outdoorScene)
-			if err != nil {
-				log.Printf("Could not get desired brightness for light '%s': %s\n", light.Name, err)
-				continue
-			}
-
-			log.Printf("Setting desired brightness for light '%s' to %f%%\n", light.Name, desiredBrightnessPercent)
-			for _, address := range light.InsteonOptions.Addresses {
-				addr := insteon.Address{}
-				err := addr.UnmarshalText([]byte(address))
-				if err != nil {
-					log.Printf("Could not parse insteon address for light '%s', address %s: %s\n", light.Name, address, err)
-					continue
-				}
-
-				device, err := modem.Network.Connect(addr)
-				if err != nil {
-					log.Printf("Could not connect to device at address %s: %s\n", address, err)
-					continue
-				}
-
-				if dimmer, ok := device.(insteon.Dimmer); ok {
-					// Determine the onLevel
-					onLevel := int(desiredBrightnessPercent * 255)
-					log.Printf("Calculated onLevel for device at address %s is %d\n", address, onLevel)
-
-					// Set the default onLevel
-					err = dimmer.SetDefaultOnLevel(onLevel)
-					if err != nil {
-						log.Printf("Could not set default OnLevel for device at address %s: %s\n", address, err)
-					}
-
-					// If the light is turned on, optionally adjust it's brightness
-					currentOnLevel, err := dimmer.Status()
-					if err != nil {
-						log.Printf("Could not determing current OnLevel for device at address %s: %s\n", address, err)
-						continue
-					}
-					if currentOnLevel > 0 && ((!outdoorScene.AfterMeridian && currentOnLevel < onLevel) || (outdoorScene.AfterMeridian && currentOnLevel > onLevel)) {
-						log.Printf("Adjusting current OnLevel of device at address %s to %d\n", address, onLevel)
-						err = dimmer.OnAtRamp(onLevel, 60)
-						if err != nil {
-							log.Printf("Could not adjust OnLevel of device at address %s: %s\n", address, err)
-							continue
-						}
-					}
-				} else {
-					log.Printf("Device at address %s is not a dimmer", address)
-					continue
-				}
-			}
-		}
-
-		time.Sleep(15 * time.Minute)
+	// Wait for interrupt or thread death
+	intChan := make(chan os.Signal, 1)
+	signal.Notify(intChan, os.Interrupt)
+	select {
+	case <-lightControlDone:
+		os.Exit(1)
+	case <- intChan:
+		log.Println("Interrupt caught!")
+		os.Exit(0)
 	}
 }
 
